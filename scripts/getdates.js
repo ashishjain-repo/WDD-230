b977883722dc98abad687705b06658751fef8cb2
Ashish Jain <ashishjain99@yahoo.com>
2024-01-28 14:00:19 -0700

    updates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,6 @@
 let oLastModif = new Date(document.lastModified);
 let currentYear = new Date().getFullYear()  // returns the current year
+function oLastModif()
+{
+    document.write(oLastModif)
+}
