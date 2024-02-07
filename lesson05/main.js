8c27c9d6dced40ed3255d2443e9f200796a2656c
Ashish Jain <ashishjain99@yahoo.com>
2024-02-07 13:45:10 -0700

    Update the JS later and Check

--- lesson05/main.js
+++ lesson05/main.js
@@ -3,11 +3,20 @@ const button = document.querySelector('#button');
 const list = document.querySelect('#list');
 
 button.addEventListener('click',function()
-																							{
+{
 	if (input.value != '')
 		{
 			const li = document.createElement('li');
 			const deleteButton = document.createElement('button');
-			
+            li.textContent = input.value;
+            deleteButton.textContent= '❌';
+            li.append(deleteButton);
+            list.append(li);
 		}
 })
+deleteButton.addEventListener('click',function()
+{
+    list.removeChild(li);
+    input.focus();
+
+});
