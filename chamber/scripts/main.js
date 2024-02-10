091f896a2efc3b3b586cf53248496f66eb498716
Ashish Jain <ashishjain99@yahoo.com>
2024-02-10 13:00:45 -0700

    js fixed

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -2,14 +2,23 @@ function displayHamburger()
 {
     var hamburger = document.getElementById("mobile-nav");
     var gridToggle = document.getElementById("navbar");
-    if (hamburger.style.display == "none")
+    var screenWidth = screen.availWidth;
+    if (hamburger.style.display == "none" && screenWidth > 480)
+    {
+        hamburger.style.display = "grid";
+        gridToggle.style.gridTemplateColumns = "10em 20em 10em 8em";
+        gridToggle.style.gridTemplateRows = "6em 100%";
+    }
+    else if (hamburger.style.display == "none" && screenWidth <= 480)
     {
         hamburger.style.display = "grid";
         gridToggle.style.gridTemplateColumns = "5em 15em";
+        gridToggle.style.gridTemplateRows = "6em 100%";
+
     }
     else
     {
         hamburger.style.display = "none";
-        gridToggle.style,gridTemplateColumns = "5em";
+        // gridToggle.style.gridTemplateColumns = "10em 20em 10em 8em";
     }
 }
