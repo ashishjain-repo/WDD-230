5ed960dd6281fc7f651dd76b88ba60bcaaf528f3
Ashish Jain <ashishjain99@yahoo.com>
2024-02-10 13:33:44 -0700

    larger function added need more designing

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -3,22 +3,30 @@ function displayHamburger()
     var hamburger = document.getElementById("mobile-nav");
     var gridToggle = document.getElementById("navbar");
     var screenWidth = screen.availWidth;
-    if (hamburger.style.display == "none" && screenWidth > 480)
+    if (hamburger.style.display == "none" && (screenWidth > 480 && screenWidth < 768) )
     {
         hamburger.style.display = "grid";
         gridToggle.style.gridTemplateColumns = "10em 20em 10em 8em";
         gridToggle.style.gridTemplateRows = "6em 100%";
+        console.log("768");
     }
     else if (hamburger.style.display == "none" && screenWidth <= 480)
     {
         hamburger.style.display = "grid";
         gridToggle.style.gridTemplateColumns = "5em 15em";
         gridToggle.style.gridTemplateRows = "6em 100%";
+        console.log("480");
 
+    }
+    else if((hamburger.style.display == "none" && screenWidth >= 1024))
+    {
+        hamburger.style.display = "flexbox";
+        console.log("1024");
     }
     else
     {
         hamburger.style.display = "none";
         // gridToggle.style.gridTemplateColumns = "10em 20em 10em 8em";
+        console.log("none");
     }
 }
