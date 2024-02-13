fe49085fe47f8fde6c32c80c1e0335a1b6b6d456
Ashish Jain <ashishjain99@yahoo.com>
2024-02-12 20:47:46 -0700

    mobile version structure done

--- temp/scripts/main.js
+++ temp/scripts/main.js
@@ -2,3 +2,5 @@ function toggleMenu() {
     const navbar = document.getElementById('navbar');
     navbar.style.display = (navbar.style.display === 'none' || navbar.style.display === '') ? 'block' : 'none';
   }
+
+document.getElementById('last-modified').textContent = document.lastModified;
