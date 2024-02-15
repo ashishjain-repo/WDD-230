3f1dc749f5016aeb16f28cfed7b107b566cd1255
Ashish Jain <ashishjain99@yahoo.com>
2024-02-14 21:00:29 -0700

    new images added

--- temp/scripts/main.js
+++ temp/scripts/main.js
@@ -4,3 +4,27 @@ function toggleMenu() {
   }
 
 document.getElementById('last-modified').textContent = document.lastModified;
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const rootSwitchToggle = document.getElementById("rootSwitchToggle");
+  const imageToSwitch = document.getElementById("hero");
+  
+  rootSwitchToggle.addEventListener("click", function() {
+    // Check if the current root theme is default or additional
+    const isDefault = document.documentElement.getAttribute('data-theme') !== 'dark';
+
+    // Toggle between default and additional root styles
+    if (isDefault) {
+      // Apply additional root styles
+      document.documentElement.setAttribute('data-theme', 'dark');
+      // Change image source for dark mode
+      imageToSwitch.src = "./images/logo.png";
+    } else {
+      // Apply default root styles
+      document.documentElement.removeAttribute('data-theme');
+      // Change image source for default mode
+      imageToSwitch.src = "./images/backgroud-main.webp";
+    }
+  });
+});
