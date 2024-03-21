338270bdcc7906de4c9abf26bde2e9fcbe179a85
Ashish Jain <ashishjain99@yahoo.com>
2024-03-21 08:33:25 -0600

    Meeting feature added

--- chamber/scripts/meet.js
+++ chamber/scripts/meet.js
@@ -1,28 +1,42 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     var today = new Date();
-    var dayOfWeek = today.getDay(); // Get the current day of the week (0 for Sunday, 1 for Monday, etc.)
+    var dayOfWeek = today.getDay(); 
     var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    // Check if it's Monday, Tuesday, or Wednesday
+    
     if (dayOfWeek >= 1 && dayOfWeek <= 3) {
-        // Create the banner element
+    
         var banner = document.createElement('div');
         banner.classList.add('banner');
 
-        // Create the text content
+    
         var message = document.createElement('p');
         message.textContent = 'Join us for the Chamber of Commerce meet and greet on ' + daysOfWeek[3] + ' at 7:00 pm!';
 
-        // Append the text content to the banner
+    
+        var closeButton = document.createElement('span');
+        closeButton.classList.add('close');
+        closeButton.textContent = 'X'; 
+
+    
+        closeButton.addEventListener('click', function () {
+            banner.style.display = 'none';
+        });
+
+        
+        message.insertBefore(closeButton, message.firstChild);
+
+        
         banner.appendChild(message);
 
-        // Get the banner container element
+        
         var bannerContainer = document.getElementById('bannerContainer');
 
-        // Append the banner to the banner container
+        
         bannerContainer.appendChild(banner);
 
-        // Show the banner
+        
         banner.style.display = 'block';
     }
 });
