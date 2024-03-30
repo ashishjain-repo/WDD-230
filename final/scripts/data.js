f4620bcb606684ba76b565726dcc337526c6664e
Ashish Jain <ashishjain99@yahoo.com>
2024-03-30 12:24:10 -0600

    Form JS fixed

--- final/scripts/data.js
+++ final/scripts/data.js
@@ -1,5 +1,5 @@
 // const url = 'https://www.fruityvice.com/api/fruit/all';
-const url = './data/data.json'
+const url = "./data/data.json";
 const fruits = [2, 4, 6, 8, 15, 22, 34];
 const returnFruits = new Array();
 
@@ -8,34 +8,34 @@ async function getFruits() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            var classes = ['choice-1', 'choice-2', 'choice-3'];
+            var classes = ["choice-1", "choice-2", "choice-3"];
             var returnFruits = new Array();
-            classes.forEach(element => {
+            classes.forEach((element) => {
                 const selectElement = document.getElementById(element);
-                fruits.forEach(fruitIndex => {
-                    const option = document.createElement('option');
+                fruits.forEach((fruitIndex) => {
+                    const option = document.createElement("option");
                     option.value = fruitIndex;
                     option.textContent = data[fruitIndex].name;
-                    if (element == 'choice-1') {
+                    if (element == "choice-1") {
                         returnFruits.push(data[fruitIndex]);
                     }
                     selectElement.appendChild(option);
                 });
             });
             return returnFruits;
-        }
-        else {
+        } else {
             throw Error(await response.text());
         }
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 }
 
 export const returnFruit = [];
-getFruits().then(element => {
-    returnFruit.push(element)
-}).catch(error => {
-    console.error(error);
-});
+getFruits()
+    .then((element) => {
+        returnFruit.push(element);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
--- final/scripts/form.js
+++ final/scripts/form.js
@@ -1,22 +1,23 @@
 const dataFull = new Array();
 
 fetch('./data/data.json')
-  .then(response => response.json())
-  .then(data => {
-      // Process the fetched data
-      dataFull.push(data)
-})
-.catch(error => {
-    console.error('Error fetching data:', error);
-});
+    .then(response => response.json())
+    .then(data => 
+    {
+        // Process the fetched data
+        dataFull.push(data)
+    })
+    .catch(error => 
+    {
+        console.error('Error fetching data:', error);
+    });
 
 console.log(dataFull)
-/* const queryString = window.location.search;
+const queryString = window.location.search;
 
-// Parse the query string into an object
 const urlParams = new URLSearchParams(queryString);
 
-// Iterate over the parameters and log them to the console
-urlParams.forEach((value, key) => {
+urlParams.forEach((value, key) => 
+{
     console.log(`${key}: ${value}`);
-}); */
+});
